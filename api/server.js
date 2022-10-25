d3b3834c57b41ae17ec61597ae0f94e71776bb94
XieMS <xms881126@gmail.com>
2022-10-25 15:49:05 +0800

    router

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/message", (req, res) => {
+  res.send("message");
+});
+
+app.listen(3000);
